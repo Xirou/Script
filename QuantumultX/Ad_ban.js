74f459cc1040b5d8cd71a1870d43f739825dc805
Xirou <54771724+Xirou@users.noreply.github.com>
2021-01-21 22:10:37 +0800

    Update Ad_ban.js

--- QuantumultX/Ad_ban.js
+++ QuantumultX/Ad_ban.js
@@ -18,8 +18,11 @@ hostname = m5.amap.com, *.sinaimg.cn, img.ithome.com,img.alicdn.com, gw.alicdn.c
 ^https:\/\/gw\.alicdn\.com\/imgextra\/i3 url 307 https://images.unsplash.com/photo-1606909684351-dadc944fd0f1?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80
 
 # > 小米运动去广告
-
+#底部横幅
+^https:\/\/api-mifit-cn2\.huami\.com\/discovery\/mi\/cards url reject
+#主页弹窗
 ^https:\/\/api-mifit-cn2\.huami\.com\/discovery\/mi\/cards\/homepage_popup url reject
+#开屏跳窗
 ^https:\/\/api-mifit-cn2\.huami\.com\/discovery\/mi\/cards\/startpage_ad url reject
 
 ^https:\/\/app\.10086\.cn\/biz-orange\/DH\/firstPage\/iconBanner$ url reject-img
