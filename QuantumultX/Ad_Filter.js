548a98b891cdf9fbd1f327928cb8af0983a0d0ee
Xirou <54771724+Xirou@users.noreply.github.com>
2021-01-27 00:38:09 +0800

    Update Ad_Filter.js

--- QuantumultX/Ad_Filter.js
+++ QuantumultX/Ad_Filter.js
@@ -1,8 +1,9 @@
+host, mmcomm.qpic.cn, reject
 host, v.didi.cn, reject-img
 host, img-hxy021.didistatic.com, reject-img
 host, pt-starfile.didistatic.com, reject-img
 host, shop-gw.chengxinyouxuan.com, reject-img
-host, img-ys011.didistatic.com, reject-img
+host, img-ys011.didistatic.com, rejec-img
 host, msgacs.m.taobao.com, reject-img
 host-keyword, mobiledispatch, reject
 host, restapi.amap.com, reject
