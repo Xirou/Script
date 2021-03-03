afae164d2665440315b0fa963aae931544fad0e5
Xirou <54771724+Xirou@users.noreply.github.com>
2021-03-04 00:05:26 +0800

    Update VerifyReceipt_Unlock.js

--- QuantumultX/VerifyReceipt_Unlock.js
+++ QuantumultX/VerifyReceipt_Unlock.js
@@ -1,6 +1,9 @@
 //VVEBO
 ^https:\/\/buy\.itunes\.apple\.com\/verifyReceipt url script-response-body https://raw.githubusercontent.com/Xirou/Script/master/QuantumultX/File/vvebo.js
 
+//剪影-RCam-Picsew-Gear-Geis-VCUS
+^https:\/\/buy\.itunes\.apple\.com\/verifyReceipt url script-response-body https://raw.githubusercontent.com/Xirou/Script/master/QuantumultX/Receipt.js
+
 //Videoleap
 ^https:\/\/buy\.itunes\.apple\.com\/verifyReceipt url script-response-body https://raw.githubusercontent.com/Xirou/Script/master/QuantumultX/File/Videoleap.js
 
@@ -10,6 +13,4 @@
 //shred
 ^https:\/\/api\.shred\.app\/verifyReceipt url script-response-body https://raw.githubusercontent.com/langkhach270389/Scripting/master/shred.js
 
-//RCam
-//^https:\/\/buy\.itunes\.apple\.com\/verifyReceipt url script-response-body https://raw.githubusercontent.com/ddgksf2013/Cuttlefish/master/Crack/receipt.js
 
