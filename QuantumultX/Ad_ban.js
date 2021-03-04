064e269f91f88d817fa7253f84fdce2b4fa8c490
Xirou <54771724+Xirou@users.noreply.github.com>
2021-03-04 23:24:59 +0800

    Update Ad_ban.js

--- QuantumultX/Ad_ban.js
+++ QuantumultX/Ad_ban.js
@@ -24,7 +24,7 @@ hostname = wx3.sinaimg.cn,userlink.alicdn.com, ww2.sinaimg.cn,api.ithome.com,iss
 ^http:\/\/ww4\.sinaimg\.cn\/large url reject
 ^http:\/\/ww5\.sinaimg\.cn\/large url reject
 ^https:\/\/img\.ithome\.com\/m\/images\/app\/task-tb-hb\.png$ url rejrct
-^https:\/\/api\.ithome\.com\/json\/slide\/index$ url rejrct
+^https:\/\/api\.ithome\.com\/json\/slide\/index$ url reject
 ^https:\/\/userlink\.alicdn\.com\/matrix_app\/ios\/matrix_app_config\.json$ url reject
 
 # > 菜鸟裹裹去除首页广告
