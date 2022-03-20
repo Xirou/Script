880a7a6c481b6b97c182ac8915b67d469ae454a2
Xirou <54771724+Xirou@users.noreply.github.com>
2022-03-21 04:41:22 +0800

    Update DDCX.js

--- QuantumultX/DDCX.js
+++ QuantumultX/DDCX.js
@@ -9,16 +9,18 @@ hostname= pt-starimg.didistatic.com, security.xiaojukeji.com, guard.sec.xiaojuke
 ^https:\/\/guard\.sec\.xiaojukeji\.com\/api\/guard\/psg\/v2\/getShieldStatus url reject
 
 // > 02 底部打车&代价推荐
-^https:\/\/conf\.diditaxi\.com\.cn\/one\/page url reject
+^https:\/\/conf\.diditaxi\.com\.cn\/one url reject
 
 // > 03 福利专区-金融服务-公交-骑车-滴滴乐园
 ^https:\/\/conf\.diditaxi\.com\.cn\/nav\/widget url reject
 
 // > 04 接送机-优惠商城
-^https:\/\/api\.udache\.com\/gulfstream\/pre-sale\/v1\/other\/pGetSceneList url reject
+^https:\/\/api\.udache\.com\/gulfstream url reject
 
 // > 05 主页领任务
-^https:\/\/conf\.diditaxi\.com\.cn\/api\/(dynamic|fusion|dynamicmodule) url reject
+^https:\/\/conf\.diditaxi\.com\.cn\/api\/(component|fusion|dynamicmodule|usercenter) url reject
+
+^https:\/\/conf\.diditaxi\.com\.cn\/dynamic url reject
 
 // > 06 主页 非主流式悬浮挂件
 ^https:\/\/res\.xiaojukeji\.com\/resapi\/activity\/mget url reject
