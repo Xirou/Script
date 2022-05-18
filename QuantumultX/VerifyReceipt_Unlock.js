11bcc362f2620c6c05a787d9a26f71120bed155a
Xirou <54771724+Xirou@users.noreply.github.com>
2022-05-19 07:16:30 +0800

    Update VerifyReceipt_Unlock.js

--- QuantumultX/VerifyReceipt_Unlock.js
+++ QuantumultX/VerifyReceipt_Unlock.js
@@ -1,8 +1,12 @@
-//VVEBO
-^https:\/\/buy\.itunes\.apple\.com\/verifyReceipt url script-response-body https://raw.githubusercontent.com/Xirou/Script/master/QuantumultX/File/vvebo.js
+//BUYHACK Time:5.19
 
-//剪影-RCam-Picsew-Gear-Geis-VCUS
+hostname= buy.itunes.apple.com
+ 
+# > Buyhack Pro
+^https:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body https://raw.githubusercontent.com/langkhach270389/Quantumult-X-LK/master/Scripts/langkhach/verify_receipt.js
+
+//JU>RCam>Picsew>Gear>Geis>Vcus Pro
 ^https:\/\/buy\.itunes\.apple\.com\/verifyReceipt url script-response-body https://raw.githubusercontent.com/Xirou/Script/master/QuantumultX/Receipt.js
 
-//Videoleap
-^https:\/\/buy\.itunes\.apple\.com\/verifyReceipt url script-response-body https://raw.githubusercontent.com/Xirou/Script/master/QuantumultX/File/Videoleap.js
+# > Vvebo Pro
+^https:\/\/buy\.itunes\.apple\.com\/verifyReceipt url script-response-body https://raw.githubusercontent.com/Xirou/Script/master/QuantumultX/File/vvebo.js
