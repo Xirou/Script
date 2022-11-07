43684f0e63b3f0bf899087bf13317b5b14377a72
Xirou <54771724+Xirou@users.noreply.github.com>
2022-11-07 11:48:38 +0800

    Update Ad_Rewrite.js

--- QuantumultX/Ad_Rewrite.js
+++ QuantumultX/Ad_Rewrite.js
@@ -33,8 +33,8 @@ hostname = *.sycdn.kuwo.cn, img.youpin.mi-img.com,userlink.alicdn.com, api.ithom
 # > 菜鸟裹裹去除首页广告
 ^https:\/\/gw\.alicdn\.com\/tfs url reject
 ^https:\/\/img\.alicdn\.com\/imgextra\/i1 url reject
-^https:\/\/files\.alicdn\.com\/tpsservice url 307 https://images.unsplash.com/photo-1606909684351-dadc944fd0f1?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80
-^https:\/\/gw\.alicdn\.com\/imgextra\/i3 url 307 https://images.unsplash.com/photo-1606909684351-dadc944fd0f1?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80
+//^https:\/\/files\.alicdn\.com\/tpsservice url 307 https://images.unsplash.com/photo-1606909684351-dadc944fd0f1?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80
+//^https:\/\/gw\.alicdn\.com\/imgextra\/i3 url 307 https://images.unsplash.com/photo-1606909684351-dadc944fd0f1?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80
 
 # > 小米运动去广告
 #底部横幅
