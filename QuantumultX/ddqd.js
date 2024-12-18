fc6b2723f3fc0755530608bd2ea04ba8069433aa
Xirou <54771724+Xirou@users.noreply.github.com>
2024-12-19 01:36:18 +0800

    Update ddqd.js

--- QuantumultX/ddqd.js
+++ QuantumultX/ddqd.js
@@ -1,8 +1,13 @@
-var obj = JSON.parse($response.body);
-console.log("Original Response: ", $response.body);  // 输出原始响应体
+var body = $response.body;  // 获取响应体
+var obj = JSON.parse(body);  // 解析响应体为对象
 
-if (obj && obj.data) {
-    obj.data.timeStamp = 1734539835894;
-    obj.data.proEndDate = "2099-01-01T00:00:00.000+0000";
-    obj.data.pro = true;
-$done({ body: JSON.stringify(obj) });
+// 设置修改字段
+obj.data.proEndDate = "2099-01-01T00:00:00.000+0000";  // 设置 proEndDate 为 2099-01-01
+obj.data.needSubscribe = false;  // 设置 needSubscribe 为 false
+obj.data.pro = true;  // 设置 pro 为 true
+
+// 将修改后的对象转回 JSON 字符串
+body = JSON.stringify(obj);
+
+// 返回修改后的响应
+$done({body: body});  // Loon 需要使用 {body: body} 来返回修改后的响应体
