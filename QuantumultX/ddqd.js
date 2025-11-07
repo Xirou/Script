e8abface0670f191fc8cc3d0118e9f7884a14ae6
Xirou <54771724+Xirou@users.noreply.github.com>
2025-11-07 10:26:23 +0800

    更新 ddqd.js

--- QuantumultX/ddqd.js
+++ QuantumultX/ddqd.js
@@ -1,25 +1,16 @@
-var body = $response.body;  // 获取响应体
-var obj = null;
-
-try {
-    obj = JSON.parse(body);  // 解析响应体为对象
-} catch (e) {
-    console.log("Failed to parse response body", e);
-    $done(body);  // 如果解析失败，直接返回原始响应
-    return;
-}
-
-if (obj && obj.data) {
-    // 设置修改字段
-    obj.data.proEndDate = "2099-01-01T00:00:00.000+0000";  // 设置 proEndDate 为 2099-01-01
-    obj.data.needSubscribe = false;  // 设置 needSubscribe 为 false
-    obj.data.pro = true;  // 设置 pro 为 true
-
-    // 将修改后的对象转回 JSON 字符串
-    body = JSON.stringify(obj);
-    console.log("Modified Body: ", body);  // 打印修改后的响应体
-    $done({ body });  // Loon 需要使用 {body: body} 来返回修改后的响应体
-} else {
-    console.log("Invalid response format");
-    $done(body);  // 如果响应体结构不符合预期，直接返回原始响应
-}
+var vvip=[
+    "body",              
+    "parse",            
+    "proEndDate",       
+    "8888-08-08T00:00:00.000+0000", 
+    "needSubscribe",     
+    "pro",             
+    "stringify"
+];
+var body=$response[vvip[0]];
+var obj=JSON[vvip[1]](body);
+obj[vvip[2]]= vvip[3];
+obj[vvip[4]]= false;
+obj[vvip[5]]= true;
+body= JSON[vvip[6]](obj);
+$done({body})
